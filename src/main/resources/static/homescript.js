866cefbb9a8cc8f65eba7fdd8fed8afb0585b5c4
Joao Pedro Silva Correa <joaocorrea@MacBook-Air-de-Joao.local>
2025-09-03 20:37:45 -0300

    subindo arquivos html, css e js da pagina home

---
+++ src/main/resources/static/homescript.js
@@ -0,0 +1,5 @@
+function abrirPagina(pagina) {
+    alert('Redirecionando para a página de ' + pagina.toUpperCase() + '.');
+    // No Spring Boot, redirecionar o usuário assim:
+    // window.location.href = '/' + pagina;
+}
