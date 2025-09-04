eb36f1fa205fe4d9e10df921bb868f6a0b62f91d
Joao Pedro Silva Correa <joaocorrea@MacBook-Air-de-Joao.local>
2025-09-04 19:16:52 -0300

    subindo html da pagina home e configurando banco de dados para começar a fazer o primeiro form

---
+++ src/main/java/com/github/paiaao/biblioteca/service/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.github.paiaao.biblioteca.service;
+
+import com.github.paiaao.biblioteca.model.Categoria;
+import com.github.paiaao.biblioteca.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public List<Categoria> listarTodas() {
+        return categoriaRepository.findAll();
+    }
+}
