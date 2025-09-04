a54f37c30025ec44df2d4195d6485406381d0225
Joao Pedro Silva Correa <joaocorrea@MacBook-Air-de-Joao.local>
2025-09-04 20:07:48 -0300

    arrumei os caminhos da pagina home.html e form.html

--- src/main/resources/static/homescript.js
+++
@@ -1,5 +0,0 @@
-function abrirPagina(pagina) {
-    alert('Redirecionando para a página de ' + pagina.toUpperCase() + '.');
-    // No Spring Boot, redirecionar o usuário assim:
-    // window.location.href = '/' + pagina;
-}
